8fa182f1fbfebe02ee626481ca49bb4070f6e15a
iamcharlie17 <riyadhosen40@gmail.com>
2025-01-20 22:54:51 +0600

    fiej

--- frontend/app/layouts/Mainlayout.tsx
+++ frontend/app/layouts/Mainlayout.tsx
@@ -3,13 +3,15 @@ import Footer from "~/components/Footer/Footer";
 import NavBar from "~/components/NavBar/NavBar";
 
 const Mainlayout = () => {
-    return (
-        <div>
-            <NavBar/>
-            <Outlet/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <div className="min-h-[calc(100vh-64px-48px)] md:min-h-[calc(100vh-76px-48px)]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Mainlayout;
