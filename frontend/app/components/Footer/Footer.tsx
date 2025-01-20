a74f6ad84d54c9752645a3794f4c961cc388fab7
iamcharlie17 <riyadhosen40@gmail.com>
2025-01-20 19:56:00 +0600

    responsive

--- frontend/app/components/Footer/Footer.tsx
+++ frontend/app/components/Footer/Footer.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-100 text-black p-4 text-center">
       <div>
-          <h1 className="text-xs">Developed by Charlie</h1>
+          <h1 className="text-xs">&copy; Charlie. All rights reserved.</h1>
       </div>
     </footer>
   );
--- frontend/app/components/NavBar/NavBar.tsx
+++ frontend/app/components/NavBar/NavBar.tsx
@@ -1,34 +1,69 @@
 import { Link, NavLink } from "react-router";
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navLinks = [
-    {
-      url: "/",
-      title: "Home",
-    },
-    {
-      url: "/about",
-      title: "About",
-    },
-    {
-      url: "/contact",
-      title: "Contact",
-    },
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/contact", title: "Contact" },
   ];
+
+  const handleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="flex justify-between items-center bg-white text-black p-6 shadow-lg">
-      <div>
-        <h1 className="font-bold text-xl">AUTHENTICATION</h1>
-      </div>
-      <div className="flex gap-8">
+    <div className="relative">
+      <nav className="flex justify-between items-center bg-white text-black p-4 md:p-6 shadow-lg">
+        <div className="block md:hidden">
+          <button onClick={handleMenu}>
+            <IoMenu size={25} />
+          </button>
+        </div>
+
+        <div>
+          <Link to="/">
+            <h1 className="font-bold text-lg md:text-xl">AUTHENTICATION</h1>
+          </Link>
+        </div>
+
+        <div className="hidden md:flex gap-8">
+          {navLinks.map((link, idx) => (
+            <div key={idx}>
+              <NavLink
+                to={link?.url}
+                className={({ isActive }) =>
+                  isActive ? "font-bold underline" : "hover:font-bold"
+                }
+              >
+                {link?.title}
+              </NavLink>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <Link to="/login">
+            <button className="bg-black text-white px-3 md:px-4 py-1 md:py-2 rounded-md uppercase font-semibold md:font-bold hover:bg-gray-800 transition duration-300">
+              Login
+            </button>
+          </Link>
+        </div>
+      </nav>
+
+      <div
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } md:hidden flex-col justify-center items-center absolute gap-4 left-0 w-full bg-white z-10 shadow-lg p-8`}
+      >
         {navLinks.map((link, idx) => (
           <div key={idx}>
             <NavLink
+              onClick={() => setIsOpen(false)}
               to={link?.url}
               className={({ isActive }) =>
-                isActive
-                  ? "font-bold underline"
-                  : "hover:font-bold"
+                isActive ? "font-bold underline" : "hover:font-bold"
               }
             >
               {link?.title}
@@ -36,14 +71,7 @@ const NavBar = () => {
           </div>
         ))}
       </div>
-      <div>
-        <Link to={"/login"}>
-          <button className="bg-black text-white px-4 py-2 rounded-md uppercase font-bold">
-            Login
-          </button>
-        </Link>
-      </div>
-    </nav>
+    </div>
   );
 };
 
